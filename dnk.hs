42c6bee270eaf071d531f6347aa49542079e2aa6
Your Name <you@example.com>
2017-07-07 18:46:13 +0300

    Добавил ввод/вывод из консоли

--- dnk.hs
+++ dnk.hs
@@ -4,10 +4,17 @@ not' :: Bool' -> Bool'
 not' False' = True'
 not' True' = False'
 
-data DNK = A | T | G | C deriving Show
+data DNK = A | T | G | C deriving (Show,Read)
 
 pair :: DNK -> DNK
 pair A = T
 pair T = A
 pair G = C
 pair C = G
+
+f x = show $ pair $ read [x]
+
+main = do
+	x <- getLine
+	let y = concat (map f x)
+	putStrLn y
