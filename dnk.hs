4fa21f44b5dcbbf7580838f11b1d37cdfa29f30b
Your Name <you@example.com>
2017-07-07 16:35:12 +0300

    Добавил ДНК

--- dnk.hs
+++ dnk.hs
@@ -3,3 +3,11 @@ data Bool' = False' | True' deriving Show
 not' :: Bool' -> Bool'
 not' False' = True'
 not' True' = False'
+
+data DNK = A | T | G | C deriving Show
+
+pair :: DNK -> DNK
+pair A = T
+pair T = A
+pair G = C
+pair C = G
